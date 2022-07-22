7f26bc12b3984db21c9c62180e2840c8d505a9be
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-07-22 18:26:59 +0200

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,34 +1,30 @@
-let string = "";
-let currentResult = 0;
-
 const add = function(a, b) {
-	string = `${a} + ${b}`;
-  return a + b;
+ return a + b;
 };
 
 const subtract = function(a, b) {
-	string = `${a} - ${b}`;
-  return a - b;
+ return a - b;
 };
 
-const sum = function(a, b) {
-	string = `${a} + ${b}`;
-  return a + b;
-};
-
+const sum = function(numbers) {
+ return numbers.reduce((a,b) => a + b, 0);
+}
 const multiply = function(a, b) {
-  string = `${a} * ${b}`;
   return a * b;
 
 };
 
-const power = function() {
+const power = function(base, power) {
+  return Math.pow(base, power);
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) 
+    return 1;
+    return n * factorial(n-1);
+  }
 	
-};
 
 // Do not edit below this line
 module.exports = {
