9740f8b9b6d84d4e4158f503514231b65435f9bc
d0bsidian <101651861+d0bsidian@users.noreply.github.com>
2022-07-25 13:00:46 +0200

    25

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+const getTheTitles = function(arr) {
+    return arr.map(book => book.title);
 };
+ 
+    
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
